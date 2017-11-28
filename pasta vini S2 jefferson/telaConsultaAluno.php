a7ce4448af793e11fcfc0aba90d55f9de58c861c
Italo Melo <italord.melo@gmail.com>
2017-11-28 15:23:27 -0200

    alterei algumas palavras

--- pasta vini S2 jefferson/telaConsultaAluno.php
+++ pasta vini S2 jefferson/telaConsultaAluno.php
@@ -15,10 +15,8 @@
   <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>            
   <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
-           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
-           <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script> 
-           <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>            
-           <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />    
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />         
+           
   <link rel = "stylesheet" href="_css/estilos1.css">
 </head>
 
